fd0bb7ab10c6f59b8f80795a699e9d785039f23c
NickCretu <155891745+NickCretu@users.noreply.github.com>
2026-02-13 15:21:07 +0200

    fix(seo): automate sitemap generation and align robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://pauch.netlify.app',
   base: '/',
   integrations: [
     tailwind(),
+    sitemap(),
   ],
   i18n: {
     defaultLocale: 'en',
